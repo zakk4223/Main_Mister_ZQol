06484245cee3f7945421da21b1808b56865b6488
Zakk <zakk@rsdio.com>
2021-02-14 04:20:26 -0500

    First commit, workflow test

---
+++ scripts/patchall.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+
+MYDIR="$(dirname "$(readlink -f "$0")")"
+MISTER_SRC=$1
+
+
+for f in $MYDIR/../patches/*.patch; do
+	PATCH_NAME="$(basename "$f")"
+	git -C $MISTER_SRC apply $f	
+	if [ $? -eq 0 ]
+	then
+		echo "Applied $f"
+	else
+		echo "PATCH FAILED: $f"
+		git -C $MISTER_SRC reset --hard HEAD --quiet
+		exit 1
+	fi
+done
